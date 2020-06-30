3fd840a769dbc58d3bce66ba730b7b2c5378a4f9
pauline-mfr <65295048+pauline-mfr@users.noreply.github.com>
2020-06-30 16:44:11 +0200

    Index du flappy 2

--- index.php
+++ index.php
@@ -2,72 +2,83 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Flappy</title>
+    <title>Flappy 2</title>
 
     <style>
     #myCanvas {background-image: url("bg_flappy.jpg"); background-repeat: no-repeat; background-size: cover;}
-    #bird {display: none;}
-    </style>
+  </style>
 
   </head>
 
   <body>
-    <img id="bird" src="bird.png" alt="">
-  <canvas id="myCanvas" width="500" height="800"></canvas>
-  <script>
+<canvas id="myCanvas" width="600" height="800"></canvas>
 
+  <script>
 
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
-  var img = document.getElementById('bird');
-  var x = canvas.width/2;
-  var y = canvas.height/2;
-  var dx = 0; // déplacement en X
-  var dy = 1; // déplacement en Y
-  var gravity = 1;
-  var speed = 0;
 
-  img.addEventListener("load", display);
-  function display(){
-    ctx.drawImage(img, 50, 25, 100,70);
+// load images
+
+ var bird = new Image();
+ var topPipe = new Image();
+ var bottomPipe = new Image();
+
+ bird.src = "bird2.png";
+ topPipe.src = "pipe_top.png";
+ bottomPipe.src = "pipe_bottom.png";
+
+ // variables
+
+var gap = 100;
+var constant = topPipe.height + gap;
+
+var bX = 50;
+var bY = 400;
+var gravity = 1;
+
+var pipe = [];
+
+pipe[0] = {
+  x : canvas.width,
+  y : 0
+};
+
+// draw images
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+for (var i = 0; i < pipe.length; i++) {
+
+  ctx.drawImage(topPipe,pipe[i].x, pipe[i].y);
+  ctx.drawImage(bottomPipe, pipe[i].x, pipe[i].y+constant); //0 + topPipe + gap
+
+  pipe[i].x--;
+
+  if (pipe[i].x == 150) {
+    pipe.push({
+      x : canvas.width,
+      y : Math.floor(Math.random()*topPipe.height)-topPipe.height
+    })
   }
+}
+
+ctx.drawImage(bird, bX, bY, 90, 70);
+//ctx.clearRect(bird, bX, bY, 90, 70);
+bY += gravity;
 
 
 
+}
+  setInterval(draw, 10);
 
-function dessineBall() {
-      ctx.beginPath();
-      ctx.arc(x, y, 10, 0, Math.PI*2);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
-  }
-  function chute() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      dessineBall();
-      x += dx;
-      y += dy;
 
-      //stop ball
-      if (y == canvas.height) {
-        dy = 0;
-      }
-  }
+//  BALL UP
+document.addEventListener("keydown", up);
 
-  //dessineBall();
-  setInterval(chute, 10);
-
- //ball up
- window.addEventListener("keydown", up, true);
-  function up(e) {
-    if (e.keyCode == "32") {
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      y = y - 50;
-      ctx.fill(x, y, 10, 0, Math.PI*2);
-    }
-  }
-
+function up(){
+  bY -= 20;
+}
 
 
 
